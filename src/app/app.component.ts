0718779e58608e6fbdfac408b7e5f8f5647135a5
Mamath68 <mathieu.stamm@gmail.Com>
2023-10-21 23:00:48 +0200

    tentative, infructueuse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Router, RouterOutlet } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html',
-    standalone: true,
-    imports: [RouterOutlet],
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  standalone: true,
+  imports: [RouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private router: Router,
+    private authService: AuthService,
+
   ) { }
+
+  ngOnInit() {
+    this.authService = this.authService;
+  }
   goToPokemonList() {
     this.router.navigate(['/pokemons']);
   }
+
 }
